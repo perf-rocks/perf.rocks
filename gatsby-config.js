c6b245ff6492f62b7df06bbc6cbc7af29ce367f4
Daniel Guillan <daniel.guillan@gmail.com>
2019-01-20 12:53:43 +0100

    chore(*): source data from Airtable

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+require('dotenv').config({
+  path: `.env`
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `perf.rocks`,
+    siteUrl: `https://perf.rocks`,
+    description: `Web performance matters. Find resources that help you build lightning fast websites.`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API_KEY,
+        tables: [
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Resources`,
+            tableView: `Published`,
+            tableLinks: [`Topic`]
+          },
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Tags`
+          }
+        ]
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,74 @@
-import React from "react"
+import React from 'react';
+import { graphql } from 'gatsby';
 
-export default () => <div>perf.rocks</div>
+const Index = ({ data }) => {
+  const siteMetadata = data.site.siteMetadata;
+  const resources = data.resources.edges.map(edge => edge.node.data);
+  const topics = data.topics.edges.map(edge => edge.node.data.Name);
+
+  return (
+    <div>
+      <h1>{siteMetadata.title}</h1>
+      <p>{siteMetadata.description}</p>
+
+      <nav>
+        <h2>Topics</h2>
+        <ul>
+          {topics.map((topic, i) => {
+            return <li key={i}>{topic}</li>;
+          })}
+        </ul>
+      </nav>
+
+      <main>
+        <h2>Resources</h2>
+        <ul>
+          {resources.map((resource, i) => {
+            return (
+              <li key={i}>
+                <a href={resource.url}>{resource.Title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query Index {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    topics: allAirtable(filter: { table: { eq: "Tags" } }) {
+      edges {
+        node {
+          data {
+            Name
+          }
+        }
+      }
+    }
+    resources: allAirtable(filter: { table: { eq: "Resources" } }) {
+      edges {
+        node {
+          data {
+            Title
+            url
+            Topic {
+              data {
+                Name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Index;
